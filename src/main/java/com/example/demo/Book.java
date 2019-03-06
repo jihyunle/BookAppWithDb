f78ddb1baac9236c133b359812994e33e1c86316
jihyunle <jennifer.jlee@outlook.com>
2019-03-06 13:42:37 -0500

    Working well

--- src/main/java/com/example/demo/Book.java
+++ src/main/java/com/example/demo/Book.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -26,11 +27,13 @@ public class Book {
     private String description;
 
     @NotNull
+    @Min(0)
     private double price;
 
     private boolean isInStock;
 
     @NotNull
+    @Min(0)
     private int numberOfBooks;
 
     public Book(){
