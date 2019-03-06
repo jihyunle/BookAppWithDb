b30054db59b69db734f6c5a2b039078004aa7719
jihyunle <jennifer.jlee@outlook.com>
2019-03-06 16:54:11 -0500

    Added comments in app properties

--- mySQL/book.sql
+++
@@ -1,5 +0,0 @@
-create database book; -- creates anew
-use book; -- selects for use
-
-select book0_.id as id1_0_, book0_.author as author2_0_, book0_.description as descript3_0_, book0_.is_in_stock as is_in_st4_0_, book0_.number_of_books as number_o5_0_, book0_.price as price6_0_, book0_.title as title7_0_ from book book0_;
-
